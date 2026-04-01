3a2cd801d0df0d837d77d78a9d968836f1a0308f
Andrexxx01 <kurniawan.andre50@gmail.com>
2026-04-01 22:02:06 +0700

    initial setup next JS

---
+++ src/app/(dashboard)/investments/page.tsx
@@ -0,0 +1,3 @@
+export default function InvestmentsPage() {
+  return <main>Investments Page</main>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <main>WealthWise Landing Page</main>;
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Button>Wealth Wise</Button>
+    </main>
+  );
 }
